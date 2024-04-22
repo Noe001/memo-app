e8705473247bfcbeb5b0b295cfc7445b0c115db3
Noe001 <goodtack530@gmail.com>
2024-04-22 23:04:31 +0900

    コード修正

--- app/javascript/users/login.js
+++ app/javascript/users/login.js
@@ -1,20 +1,19 @@
-function element() {
+window.onload = function() {
   let element = document.getElementById('circle_1');
   let radiusX = 100; // X軸半径
   let radiusY = 50;  // Y軸半径
   let centerX = 200; // 楕円の中心X座標
   let centerY = 200; // 楕円の中心Y座標
-  let angle = 0;
-}
+  let angle = 0
 
-function moveElement() {
-  element();
-  let x = centerX + Math.cos(angle) * radiusX;
-  let y = centerY + Math.sin(angle) * radiusY;
-  element.style.left = x + 'px';
-  element.style.top = y + 'px';
-  angle += 0.1; // アニメーションの速度を調整するための角度変化
-  requestAnimationFrame(moveElement);
-}
+  function moveElement() {
+    let x = centerX + Math.cos(angle) * radiusX;
+    let y = centerY + Math.sin(angle) * radiusY;
+    element.style.left = x + 'px';
+    element.style.top = y + 'px';
+    angle += 0.03; // アニメーションの速度を調整するための角度変化
+    requestAnimationFrame(moveElement);
+  }
 
-moveElement();
+  moveElement();
+}
