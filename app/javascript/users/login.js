fffb3817ea718245e99caed1f29cf96a2ab1c5a1
Noe001 <goodtack530@gmail.com>
2024-04-22 23:55:00 +0900

    要素追加

--- app/javascript/users/login.js
+++ app/javascript/users/login.js
@@ -1,9 +1,9 @@
 window.onload = function() {
   let element = document.getElementById('circle_1');
-  let radiusX = 100; // X軸半径
-  let radiusY = 50;  // Y軸半径
-  let centerX = 200; // 楕円の中心X座標
-  let centerY = 200; // 楕円の中心Y座標
+  let radiusX = 400; // X軸半径
+  let radiusY = 200;  // Y軸半径
+  let centerX = 1160; // 楕円の中心X座標
+  let centerY = 500; // 楕円の中心Y座標
   let angle = 0
 
   function moveElement() {
@@ -11,7 +11,8 @@ window.onload = function() {
     let y = centerY + Math.sin(angle) * radiusY;
     element.style.left = x + 'px';
     element.style.top = y + 'px';
-    angle += 0.03; // アニメーションの速度を調整するための角度変化
+    element.style.overflow = 'hidden';
+    angle += 0.007; // アニメーションの速度を調整するための角度変化
     requestAnimationFrame(moveElement);
   }
 
