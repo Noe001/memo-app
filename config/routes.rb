d87dca3ddff74639270b3ca6164555cb9dc3f06b
Noe001 <goodtack530@gmail.com>
2024-05-24 17:19:47 +0900

    大幅修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'login', to: 'sessions#new', as: 'new_sessions'
-  post 'login', to: 'sessions#create', as: 'create_sessions'
-  delete 'logout', to: 'sessions#destroy', as: 'destroy_sessions'
+
+  root :to => "memos#index"
+  resources :memos, only: [:index, :update, :destroy, :create]
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_062452) do
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_071119) do
+  create_table "memos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
