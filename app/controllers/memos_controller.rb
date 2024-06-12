1f16d68c86c9a74f15a532decd07d2e4be4903b2
Noe001 <goodtack530@gmail.com>
2024-06-12 10:58:22 +0900

    ログイン機能続き

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,5 @@
 class MemosController < ApplicationController
+  before_action :current_user
 
   def index
     @memo_new = Memo.new
@@ -27,6 +28,14 @@ class MemosController < ApplicationController
 
   def memos_params
     params.require(:memo).permit(:title, :description)
+  end
+
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    else
+      redirect_to new_sessions_path
+    end
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in, only: [:new, :create]
 
   def new
 
@@ -7,18 +8,33 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
-      sessions[:user_id] = @user.id
+      session[:user_id] = @user.id
       flash[:notice] = "ログインに成功しました"
-      redirect_to memo_path
+      redirect_to memos_path
     else
-      flash[:alert] - "ログインに失敗しました"
-      require actiom: "new"
+      flash[:alert] = "ログインに失敗しました"
+      render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to new_sessions_path
+    p "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"
   end
 
+  private
+
+  def logged_in
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      p "pppppppppppppppppppppppppppppppp"
+      p session[:user_id]
+      flash[:notice] = "すでにログインしています"
+      redirect_to memos_path
+    else
+      flash[:alert] = "ログインしてください"
+      redirect_to new_sessions_path
+    end
+  end
 end
