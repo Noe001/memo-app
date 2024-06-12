4a18f910c670e08582b431a3edbfab8d95c090ae
Noe001 <goodtack530@gmail.com>
2024-06-12 23:34:57 +0900

    ページが切り替わらない状態

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,21 @@
 class SessionsController < ApplicationController
 
   def new
-
   end
 
   def create
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
-      sessions[:user_id] = @user.id
-      flash[:notice] = "ログインに成功しました"
-      redirect_to memo_path
+      session[:user_id] = @user.id
+      redirect_to memos_path, notice: 'ログインしました。'
     else
-      flash[:alert] - "ログインに失敗しました"
-      require actiom: "new"
+      flash.now[:alert] = 'メールアドレスまたはパスワードが無効です。'
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to new_sessions_path
+    redirect_to new_sessions_path, notice: 'ログアウトしました。'
   end
-
 end
