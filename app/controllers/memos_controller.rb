e9b823353e27ec928293e3893fb406fe53bd579c
Noe001 <152124564+Noe001@users.noreply.github.com>
2024-06-14 00:38:45 +0900

    memos_controller.rb を更新

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -32,7 +32,7 @@ class MemosController < ApplicationController
 
   def current_user
     if session[:user_id]
-      @user = User.find[session[:user_id]]
+      @user = User.find(session[:user_id])
     else
       redirect_to new_session_path
     end
