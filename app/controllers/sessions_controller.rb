6ff4cbe9a2d8881d6c90ba708d7deae67b652501
Noe001 <goodtack530@gmail.com>
2024-06-17 16:18:15 +0900

    sessionファイル修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,17 @@
 class SessionsController < ApplicationController
-  before_action :logged_in, only: [:new, :create]
+  before_action :set_user, only: [:create, :destroy]
+  before_action :logged_in, only: [:create]
 
   def new
   end
 
   def create
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
+    if @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to memos_path, notice: 'ログインしました。'
     else
       flash.now[:alert] = 'メールアドレスまたはパスワードが無効です。'
-      render :new, status: :unprocessable_entity
+      render :new, status: :unauthorized
     end
   end
 
@@ -21,6 +21,10 @@ class SessionsController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find_by(email: params[:email])
+  end
 
   def logged_in
     if session[:user_id]
