e755ba37aa0d25896cff22a082eab19a4cfc5c49
Noe001 <goodtack530@gmail.com>
2024-06-18 23:28:47 +0900

    ログイン機能修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   before_action :set_user, only: [:create, :destroy]
-  before_action :logged_in, only: [:create]
+  before_action :logged_in, only: [:new, :create]
 
   def new
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_11_152909) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_135630) do
   create_table "memos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -21,9 +21,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_152909) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
