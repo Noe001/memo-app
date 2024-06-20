41b284c46fe4d04f79c1e87837511a72bbabd3f8
Noe001 <goodtack530@gmail.com>
2024-06-20 10:18:15 +0900

    紐づけ完了

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,5 +1,7 @@
 class Memo < ApplicationRecord
+  # ユーザー情報と関連付ける
   belongs_to :user
+  # タイトル、概要の入力は必須にする
   validates :title, presence: true
   validates :description, presence: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   # BCryptパスワードを設定して認証するためのメソッド
   has_secure_password
+  # ユーザーが作成したメモと関連付ける
+  has_many :memos
 end
