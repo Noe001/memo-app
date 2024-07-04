8f1237a19483a58f80ed4d676c55a2824ff80602
Noe001 <goodtack530@gmail.com>
2024-07-04 14:53:35 +0900

    バグ修正2

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -8,11 +8,13 @@ class MemosController < ApplicationController
     # ログインしてるユーザーが作成したメモ（サイドバーで使用）
     @memos = current_user.memos
     if params[:id]
-      # サイドバーで選択したメモ（フォーム表示に使用）
+      # サイドバーで選択またはURLで入力したメモ（フォーム表示に使用）
       @selected = Memo.find(params[:id])
-      # 一覧から選択したコンテンツの背景を変えるために使用
+      # メモをシェアした際にシェア元のユーザー名を表示
       @memo_id = params[:id]
-      @can_add = @selected != current_user.id
+      @shared_user = @selected.user.name
+      # trueかfalseを代入
+      @can_add = @selected.user_id != current_user.id
       @memo_to_add = current_user.memos.new(title: @selected.title, description: @selected.description)
     end
   end
@@ -20,9 +22,9 @@ class MemosController < ApplicationController
   def show
     @memo_new = Memo.new
     @memos = current_user.memos
+    @can_add = @selected.user_id != current_user.id
+    @shared_user = @selected.user.name
     @memo_id = @selected.id
-    # trueかfalseを代入
-    @can_add = @selected != current_user.id
     @memo_to_add = current_user.memos.new(title: @selected.title, description: @selected.description)
     render :index
   end
@@ -30,12 +32,17 @@ class MemosController < ApplicationController
   def add_memo
     @memo_to_add = current_user.memos.new(memos_params)
     if @memo_to_add.save
-      redirect_to memo_path(@memo_to_add), notice: 'メモを自分のリストに追加しました'
+      if @memo_to_add.title.blank? && @memo_to_add.description.blank?
+        @memo_to_add.destroy
+        redirect_to root_path, notice: '未入力だったため追加されませんでした'
+      else
+        redirect_to memo_path(@memo_to_add), notice: 'リストに追加しました'
+      end
     else
       @memos = current_user.memos
       @selected = Memo.find(params[:id])
       @can_add = true
-      flash.now[:alert] = 'メモの追加に失敗しました'
+      flash.now[:alert] = '追加に失敗しました'
       render :index
     end
   end
@@ -44,15 +51,17 @@ class MemosController < ApplicationController
     # ログインしたユーザーに関連付けられたメモを作成
     @memo_new = current_user.memos.build(memos_params)
     if @memo_new.save
-      # タイトルと概要が空の場合はメモを削除
+      # タイトルと本文が空の場合はメモを削除
       if @memo_new.title.blank? && @memo_new.description.blank?
         @memo_new.destroy
-        redirect_to memos_path, alert: 'タイトルと概要を入力してください'
+        flash[alert:] = 'タイトルと本文を入力してください'
       else
-        redirect_to memos_path, notice: '作成しました'
+        flash[notice:] = '作成しました'
       end
+      redirect_to memos_path
     else
-      redirect_to memos_path, alert: '保存に失敗しました'
+      flash[alert:] = '保存に失敗しました'
+      render :index
     end
   end
 
