9d08c8db6bccec014c810f4762ec8bf1748190b8
Noe001 <goodtack530@gmail.com>
2024-07-05 22:37:20 +0900

    サインアップ画面修正

--- app/javascript/users_signup.js
+++ app/javascript/users_signup.js
@@ -1,1 +1,22 @@
 history.replaceState('', '', '/signup');
+
+document.addEventListener("DOMContentLoaded", function() {
+  const nameField = document.getElementById("name")
+  const emailField = document.getElementById("email");
+  const passwordField = document.getElementById("password");
+  const passwordConfirmationField = document.getElementById("password_confirmation")
+  const createButtonField = document.getElementById("create_button");
+
+  function validateForm() {
+    if (nameField.value !=="" && emailField.value !== "" && passwordField.value !== "", passwordConfirmationField.value !== "") {
+      createButtonField.classList.add("active");
+    } else {
+      createButtonField.classList.remove("active");
+    }
+  }
+
+  nameField.addEventListener("input", validateForm);
+  emailField.addEventListener("input", validateForm);
+  passwordField.addEventListener("input", validateForm);
+  passwordConfirmationField.addEventListener("input", validateForm);
+});
